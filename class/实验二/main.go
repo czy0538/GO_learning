b80fd59d7f5f5ad11239efe212ebff53f00d469d
czy0538 <czy0538@gmail.com>
2019-07-13 09:20:46 +0800

    实验二完结撒花

--- class/实验二/main.go
+++ class/实验二/main.go
@@ -1,50 +1,25 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-	"time"
-)
-
-var n int64 = 10000000000
-var h float64 = 1.0 / float64(n)
-
-func f(a float64) float64 {
-	return 4.0 / (1.0 + a*a)
-}
-
-func chunk(start, end int64, c chan float64) {
-	var sum float64 = 0.0
-	for i := start; i < end; i++ {
-		x := h * (float64(i) + 0.5)
-		sum += f(x)
-	}
-	c <- sum * h
-}
+import "fmt"
+import "math"
 
 func main() {
+	var n int
+	fmt.Print("请输入精度:")
+	_, _ = fmt.Scanln(&n)
+	fmt.Println(Pi(n))
+}
 
-	//记录开始时间
-	start := time.Now()
-
-	var pi float64
-	np := runtime.NumCPU()
-	runtime.GOMAXPROCS(np)
-	c := make(chan float64, np)
-
-	for i := 0; i < np; i++ {
-		go chunk(int64(i)*n/int64(np), (int64(i)+1)*n/int64(np), c)
+func Pi(n int) float64 {
+	var r float64
+	ch := make(chan float64, 120)
+	for k := 0; k <= n; k++ {
+		go func(kk float64) {
+			ch <- math.Pow(-1, kk) / (2*kk + 1)
+		}(float64(k))
 	}
-
-	for i := 0; i < np; i++ {
-		pi += <-c
+	for i := 0; i < n; i++ {
+		r += <-ch
 	}
-
-	fmt.Println("Pi: ", pi)
-
-	//记录结束时间
-	end := time.Now()
-
-	//输出执行时间，单位为毫秒。
-	fmt.Printf("spend time: %vs\n", end.Sub(start).Seconds())
+	return 4 * r
 }
